1880e0ebc9a9a837edf5a2fc96b2a221dec14de5
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2022-05-22 16:46:44 +0300

    document model

--- src/main/java/com/example/demo/author/Author.java
+++ src/main/java/com/example/demo/author/Author.java
@@ -1,8 +1,12 @@
 package com.example.demo.author;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.io.Serializable;
 
+@Schema(description = "Author's representation")
 public class Author implements Serializable {
+    @Schema(description = "Author's full name", example = "J.K. Rowling", required = true)
     private String name;
 
     public String getName() {
--- src/main/java/com/example/demo/book/Book.java
+++ src/main/java/com/example/demo/book/Book.java
@@ -1,8 +1,12 @@
 package com.example.demo.book;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.io.Serializable;
 
+@Schema(description = "Book's representation")
 public class Book implements Serializable {
+    @Schema(description = "Book's title", example = "Harry Potter and the Sorcerer's Stone", required = true)
     private String title;
 
     public String getTitle() {
